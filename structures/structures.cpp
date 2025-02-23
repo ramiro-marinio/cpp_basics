06642243ce12b1d810a09d5c7dd7c58c95bfb892
Ramiro Mariño <ramiro.marinho0@gmail.com>
2025-02-23 13:35:16 -0300

    Structures

---
+++ structures/structures.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct worker {
+	unsigned short int age;
+	string name;
+};
+
+int main()
+{
+	struct worker esclavo;
+	esclavo.name = "Juam peres";
+	esclavo.age = 16;
+        cout << &esclavo << "\n";	
+	return 0;
+}
