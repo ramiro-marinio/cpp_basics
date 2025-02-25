eba6afa74ce253a578986679cca94d032b01956c
Ramiro Mariño <ramiro.marinho0@gmail.com>
2025-02-25 19:50:09 -0300

    Classes II

--- classes/classes.cpp
+++ classes/classes.cpp
@@ -6,22 +6,39 @@ class Officer {
 	public:
 		string name;
 		string rank;
-		string age;
+		int age;
+		Officer(string name, string rank, int age){
+                  this->name = name;
+                  this->rank = rank;
+                  this->age = age;
+          	}
+          	string toString(){
+                  return this->name;
+          	}
+		int getAge();
+
 };
 
 class Soldier : public Officer {
 	public:
 		string division;
 		string weapon;
-};
 
+};
 class Police : public Officer {
 	public:
 		string state;
 		string car_plate;
 };
 
+int Officer::getAge (void) {
+	return age;
+}
+
 int main()
 {
+	Officer comisario("Juancito", "Gendarme",14);
+	cout << comisario.toString() << "\n";
+	cout << comisario.getAge() << "\n";
 	return 0;
 }
